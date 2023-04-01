7de704f3911a079676171163d328152e92860c96
Gitaasv <saridevianggita15@gmail.com>
2023-04-01 12:59:49 +0700

    mengerjakan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller{
+    
+    public function Index(){
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Anggita Sari Devi 
+              saya tinggal di daerah bekasi
+              olahraga yang saya sukai bulutangkis dan kasti";
+    }
+}
